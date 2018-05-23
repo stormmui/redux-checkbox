6293d0a6b768be362031c9cd25751b6c1e69be82
Michael Angerman <stormasm@gmail.com>
2018-05-23 10:44:13 -0700

    remove icon reference

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -3,11 +3,8 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import CommentIcon from "@material-ui/icons/Comment";
 
 const styles = theme => ({
   root: {
@@ -59,11 +56,6 @@ class CheckboxList extends React.Component {
                 disableRipple
               />
               <ListItemText primary={value.name} />
-              <ListItemSecondaryAction>
-                <IconButton aria-label="Comments">
-                  <CommentIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
             </ListItem>
           ))}
         </List>
